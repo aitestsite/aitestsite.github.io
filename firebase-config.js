6673131f7c7f2b492277262d75fee9ff908cccbf
aitestsite <anoland.lou@gmail.com>
2025-02-28 16:30:26 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js (Firebase credentials)
+          // Your web app's Firebase configuration
+          const firebaseConfig = {
+            apiKey: "AIzaSyAHOmZoWC05fQ2plVjF2PoQuG-rJ8dkz4U",
+            authDomain: "aimessageboard.firebaseapp.com",
+            projectId: "aimessageboard",
+            storageBucket: "aimessageboard.firebasestorage.app",
+            messagingSenderId: "164127250910",
+            appId: "1:164127250910:web:f3d213e938cccee8f2e54f"
+          };
+
+export { firebaseConfig };
